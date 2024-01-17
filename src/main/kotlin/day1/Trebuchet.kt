547efe98b2057e8168002b89cffeb5643adeb055
LBlanka99 <losonci.blanka@mensa.hu>
2024-01-17 13:16:12 +0100

    implement solution of day1 part2

--- src/main/kotlin/day1/Trebuchet.kt
+++ src/main/kotlin/day1/Trebuchet.kt
@@ -4,6 +4,7 @@ import java.io.File
 
 fun main() {
     println(getSumOfCalibrationValues("src/main/kotlin/day1/input.txt"))
+    println(getSumOfCalibrationValuesWithWords("src/main/kotlin/day1/input.txt"))
 }
 
 fun getSumOfCalibrationValues(filename : String) : Int {
@@ -32,6 +33,56 @@ fun getSumOfCalibrationValues(filename : String) : Int {
             (10 * firstNumber) + currentLastNumber
         } else {
             (10 * firstNumber) + firstNumber
+        }
+    }
+    return sum
+}
+
+fun getSumOfCalibrationValuesWithWords(filename: String) : Int {
+    val data = File(filename).readText().trim()
+    val list = data.split("\n")
+
+    var sum = 0
+
+    val numbers = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    for (line in list) {
+        var indexOfCurrentFirstNumber = line.length
+        var currentFirstNumber = 0
+        var indexOfCurrentLastNumber = 0
+        var currentLastNumber : Int? = null
+
+        for (n in numbers.indices) {
+            val indexOfNumber = line.indexOf(numbers[n])
+            if (indexOfNumber > -1) {
+                if (indexOfNumber < indexOfCurrentFirstNumber) {
+                    currentFirstNumber = n + 1
+                    indexOfCurrentFirstNumber = indexOfNumber
+                }
+                if (line.lastIndexOf(numbers[n]) > indexOfCurrentLastNumber) {
+                    currentLastNumber = n + 1
+                    indexOfCurrentLastNumber = line.lastIndexOf(numbers[n])
+                }
+            }
+        }
+
+        for (j in line.indices) {
+            if (line[j].isDigit()) {
+                if (j < indexOfCurrentFirstNumber) {
+                    currentFirstNumber = line[j].toString().toInt()
+                    indexOfCurrentFirstNumber = j
+                }
+                if (j > indexOfCurrentLastNumber) {
+                    currentLastNumber = line[j].toString().toInt()
+                    indexOfCurrentLastNumber = j
+                }
+            }
+        }
+
+        sum += if (currentLastNumber != null) {
+            (10 * currentFirstNumber) + currentLastNumber
+        } else {
+            (10 * currentFirstNumber) + currentFirstNumber
         }
     }
     return sum
